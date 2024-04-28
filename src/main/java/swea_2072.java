eb7432945333fd86e619271c269d7d1e2b3a40c2
Jung-won-seok <ukksj0621@naver.com>
2024-04-28 20:37:31 +0900

    commit

--- src/main/java/swea_2072.java
+++ src/main/java/swea_2072.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class swea_2072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] arr = new int[10];
+        for (int i = 0; i < T; i++) {
+            int sum = 0;
+            for (int j = 0; j < 10; j++) {
+                arr[j] = sc.nextInt();
+                if (arr[j] % 2 != 0) {
+                    sum += arr[j];
+                }
+            }
+            System.out.println("#"+(i+1)+" "+sum);
+        }
+    }
 }
