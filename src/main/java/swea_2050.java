21810ac2853528b354f925d86f7e75236a94e9e7
Jung-won-seok <ukksj0621@naver.com>
2024-04-29 18:25:13 +0900

    알파벳을 숫자로 변환

--- src/main/java/swea_2050.java
+++ src/main/java/swea_2050.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class swea_2050 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char[] arr = input.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]-64 + " ");
+        }
     }
 }
