cece4efe14bb33094bdb40384c9b1117ac59b5ae
Jung-won-seok <ukksj0621@naver.com>
2024-04-30 16:29:51 +0900

    서랍의 비밀번호

---
+++ src/main/java/swea_2043.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class swea_2043 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int P = sc.nextInt();
+        int K = sc.nextInt();
+
+        System.out.println(P-K+1);
+    }
+}
