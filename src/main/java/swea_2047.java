08cd54920036ee241423f95ec393103ec1cf0cee
Jung-won-seok <ukksj0621@naver.com>
2024-04-30 16:14:21 +0900

    신문 헤드라인

---
+++ src/main/java/swea_2047.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class swea_2047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        input = input.toUpperCase();
+        System.out.println(input);
+    }
+}
