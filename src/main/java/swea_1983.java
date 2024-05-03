a4b40059dd7026e7639283a6ed570f40a2baf8d0
Jung-won-seok <ukksj0621@naver.com>
2024-05-03 17:54:47 +0900

    조교의 성적 매기기

---
+++ src/main/java/swea_1983.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class swea_1983 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            int N = sc.nextInt(); // 학생 수
+            int K = sc.nextInt(); // 알고싶은 학생 번호
+            int[] student = new int[N];
+        }
+    }
+}
