7e40bc24e99c1023095d03b1229808a269924548
Jung-won-seok <ukksj0621@naver.com>
2024-05-04 10:41:59 +0900

    조교의 성적 매기기

--- src/main/java/swea_1983.java
+++ src/main/java/swea_1983.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class swea_1983 {
@@ -7,9 +9,22 @@ public class swea_1983 {
         sc.nextLine();
         for (int test_case = 1; test_case <= T; test_case++) {
 
-            int N = sc.nextInt(); // 학생 수
-            int K = sc.nextInt(); // 알고싶은 학생 번호
-            int[] student = new int[N];
+            int N = sc.nextInt(); // 학생 수 -> 10
+            int K = sc.nextInt(); // 알고싶은 학생 번호 -> 2
+            double[] student_grade = new double[N];
+            for (int i = 0; i < N; i++) {
+                int middle = sc.nextInt();
+                int finals = sc.nextInt();
+                int homework = sc.nextInt();
+                double total_score = (0.35 * middle) + (0.45 * finals) + (0.2 * homework);
+                student_grade[i] = total_score;
+            }
+            Arrays.sort(student_grade); //등수로 정렬
+
+            for (double a : student_grade) {
+                System.out.println(a);
+            }
+
         }
     }
 }
