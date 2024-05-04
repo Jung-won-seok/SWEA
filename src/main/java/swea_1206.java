6af8f06b2990e5a2fbdf79fc2cc3f525dc60e6c8
Jung-won-seok <ukksj0621@naver.com>
2024-05-04 22:54:31 +0900

    [S/W 문제해결 기본] 1일차 - View

---
+++ src/main/java/swea_1206.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class swea_1206 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+        }
+    }
+}
