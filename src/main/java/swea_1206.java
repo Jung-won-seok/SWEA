6b67f723e001177a6b302ff3d9f57638fc57ff7f
Jung-won-seok <ukksj0621@naver.com>
2024-05-05 12:23:28 +0900

    [S/W 문제해결 기본] 1일차 - View

--- src/main/java/swea_1206.java
+++ src/main/java/swea_1206.java
@@ -3,9 +3,33 @@ import java.util.Scanner;
 public class swea_1206 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int test_case = 1; test_case <= T; test_case++) {
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int N = sc.nextInt(); //건물의 개수
+            sc.nextLine();
+            int[] hight = new int[N];
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                hight[i] = sc.nextInt();
+            } // 0 0 254 185 76 227 84 175 0 0
+            for (int j = 2; j < N - 2; j++) { //2 3 4 5 6 7
+                if (hight[j] > hight[j - 1] && hight[j] > hight[j - 2] && hight[j] > hight[j + 1] && hight[j] > hight[j + 2]) {
 
+                    int max = 0;
+                    for (int p = 1; p <= 2; p++) {
+                        if (max < hight[j - p]) {
+                            max = hight[j - p];
+                        }
+                        if (max < hight[j + p]) {
+                            max = hight[j + p];
+                        }
+                    }
+                    result += (hight[j] - max);
+
+                } else {
+                    continue;
+                }
+            }
+            System.out.println("#" + test_case + " " + result);
         }
     }
 }
