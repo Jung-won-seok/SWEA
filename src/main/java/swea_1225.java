561c27a5a3939669062340c1fd1cc9f8eb089291
Jung-won-seok <ukksj0621@naver.com>
2024-05-08 21:04:06 +0900

    [S/W 문제해결 기본] 7일차 - 암호생성기

--- src/main/java/swea_1225.java
+++ src/main/java/swea_1225.java
@@ -3,14 +3,36 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class swea_1225 {
+    static Queue<Integer> queue;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<>();
         int T = 10;
         for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            queue = new LinkedList<>();
             for (int i = 0; i < 8; i++) {
-                queue.add(sc.nextInt());
+                queue.offer(sc.nextInt());
             }
+            solve();
+            System.out.println("#"+test_case+" ");
+            for (int i = 0; i < 8; i++) {
+                System.out.print(queue.poll()+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void solve() {
+        int index = 1;
+        while (true) {
+            int cur = queue.poll() - index;
+            if (cur <= 0) {
+                queue.offer(0);
+                break;
+            } else {
+                queue.offer(cur);
+            }
+            index = (index % 5) + 1;
         }
     }
 }
