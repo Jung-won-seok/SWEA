e6f571454d234fd64302b62b2c2560f91f2c7d0d
Jung-won-seok <ukksj0621@naver.com>
2024-05-12 11:41:46 +0900

    상원이의 연속 합

--- src/main/java/swea_7510.java
+++ src/main/java/swea_7510.java
@@ -5,7 +5,21 @@ public class swea_7510 {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int test_case = 1; test_case <= T; test_case++) {
-
+            int N = sc.nextInt(); // N=15 -> [15] = [7 + 8] = [4 + 5 + 6] = [1 + 2 + 3 + 4 + 5]
+            int cnt = 0;
+            for (int i = 1; i <= N; i++) {
+                int sum = 0;
+                for (int j = i; j <= N; j++) { //1~15
+                    sum = sum + j;
+                    if (sum == N) {
+                        cnt++;
+                        break;
+                    } else if (sum > N) {
+                        break;
+                    }
+                }
+            }
+            System.out.println("#"+test_case+" "+cnt);
         }
     }
 }
