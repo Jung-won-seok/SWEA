dede8959eeb6fc3d2e05dd15bc75e4ab7c2eabf1
Jung-won-seok <ukksj0621@naver.com>
2024-05-15 22:32:12 +0900

    수의 새로운 연산

---
+++ src/main/java/swea_1493.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class swea_1493 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+        }
+    }
+
+}
