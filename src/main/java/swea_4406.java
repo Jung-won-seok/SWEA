b84ee4395550a97cfe673320bf0bc7b1d4d800e9
Jung-won-seok <ukksj0621@naver.com>
2024-05-17 12:00:28 +0900

    모음이 보이지 않는 사람

--- src/main/java/swea_4406.java
+++ src/main/java/swea_4406.java
@@ -5,7 +5,20 @@ public class swea_4406 {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int test_case = 1; test_case <= T; test_case++) {
-
+            String input = sc.next();
+            char[] arr = input.toCharArray(); //arr[] = [ c, o, n, g, r, a, t, u, l, a, t, i, o, n ]
+            int size = input.length();
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                    arr[i] = 0;
+                }
+            }
+            System.out.print("#"+test_case+" ");
+            for (int i = 0; i < size; i++) {
+                if(arr[i] != 0)
+                    System.out.print(arr[i]);
+            }
+            System.out.println();
         }
     }
 }
